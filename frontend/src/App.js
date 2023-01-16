1bd17b1f062f4a1fb1fe50b96b8a7f86c53807cf
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-01-16 13:57:45 -0500

    basic api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
 
 function App() {
+
+  let FetchTasks = async () => {
+    let response = await fetch('http://localhost:8000/api/')
+    let data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    FetchTasks()
+  }, [])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      API
+
     </div>
   );
 }
