8a53cd956731a22cb801a6f66fb64871499e91d1
sbstnbnvds <sbstnbnvdsa@gmail.com>
2023-01-16 00:21:16 -0500

    1st view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def PrimeraView(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    json_dict = {'uno': 'elnumerouno', 'dos': 'elnumerodos'}
+    return Response(serializer.data)
